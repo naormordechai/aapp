909f2695eeb5953ef1bc57f530f14807b5eed49b
naor <naormordechaix@gmail.com>
2021-09-06 16:56:41 +0300

    init

--- fronetnd/src/App.vue
+++ fronetnd/src/App.vue
@@ -1,40 +1,18 @@
 <template>
-	<div id="app">
-		<div id="nav">
-			<router-link to="/">Home</router-link> |
-			<router-link to="/about">About</router-link>
-		</div>
-		<div>
-			<div></div>
-			<p>asdasd</p>
-		</div>
-		<router-view />
-	</div>
+	<v-app>
+		<v-btn elevation="2">Click here</v-btn>
+		<v-main>
+			<router-view />
+		</v-main>
+	</v-app>
 </template>
 
 <script>
-export default {};
+export default {
+	name: 'App',
+
+	data: () => ({
+		//
+	})
+};
 </script>
-
-<style>
-#app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-}
-
-#nav {
-	padding: 30px;
-}
-
-#nav a {
-	font-weight: bold;
-	color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-	color: #42b983;
-}
-</style>
--- fronetnd/src/views/About.vue
+++ fronetnd/src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
 	<div class="about">
 		<h1>This is an about page</h1>
+		<v-btn elevation="2"></v-btn>
 	</div>
 </template>
